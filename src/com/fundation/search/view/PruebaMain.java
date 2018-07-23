68855c28e6b9bb2ac502d99961be1f3aeec60c00
lluviana <ana.mzenteno@gmail.com>
2018-07-22 20:32:45 -0400

    view ana

--- src/com/fundation/search/view/PruebaMain.java
+++
@@ -1,8 +0,0 @@
-package com.fundation.search.view;
-
-public class PruebaMain {
-    public static void main(String[] args){
-        FrameSearch f2 = new FrameSearch();
-        f2.setVisible(true);
-    }
-}
